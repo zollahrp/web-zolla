a9cd91babccf8d13f29ea7daa59871c5ab34ed01
zollahrp <141205140+zollahrp@users.noreply.github.com>
2025-07-06 22:25:17 +0700

    ceo pencarian

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://zollahrp.my.id', 
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 7000,
+};
