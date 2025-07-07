1fc6e9955d38898f9312bcbfa0f28643c2fadcc8
zollahrp <141205140+zollahrp@users.noreply.github.com>
2025-07-07 17:05:24 +0700

    add Analytics Umami

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["rcljtsnaqwlqnbwknsdm.supabase.co"],
   },
+  env: {
+    UMAMI_API_KEY: process.env.UMAMI_API_KEY,
+    UMAMI_WEBSITE_ID: process.env.UMAMI_WEBSITE_ID,
+  },
 };
 
 export default nextConfig;
--- src/app/zaga/page.js
+++ src/app/zaga/page.js
@@ -36,7 +36,22 @@ export default function Dashboard() {
 
   return (
     <AuthGuard>
-      <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+      <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="bg-white rounded-2xl shadow p-6 hover:shadow-md transition flex flex-col justify-between">
+          <div>
+            <h2 className="text-xl font-semibold mb-2">Analytics Umami</h2>
+            <p className="text-sm text-gray-600">Mau pake API mereka harus bayar :c</p>
+          </div>
+          <a
+            href="https://cloud.umami.is/websites/ae58a994-1d16-47de-b5fd-a590fd494be7"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 inline-block bg-black text-white text-sm px-4 py-2 rounded-xl hover:bg-gray-800 transition"
+          >
+            Buka Umami
+          </a>
+        </div>
+        {/* Total Views */}
         <div className="bg-white rounded-2xl shadow p-6 hover:shadow-md transition">
           <h2 className="text-xl font-semibold mb-2">Total Blog Views</h2>
           <p className="text-3xl font-bold text-orange-500">
@@ -44,24 +59,17 @@ export default function Dashboard() {
           </p>
         </div>
 
+        {/* Total Blog */}
         <div className="bg-white rounded-2xl shadow p-6 hover:shadow-md transition">
           <h2 className="text-xl font-semibold mb-2">Total Blog</h2>
-          <p className="text-3xl font-bold text-blue-500">
-            {stats.totalBlog}
-          </p>
+          <p className="text-3xl font-bold text-blue-500">{stats.totalBlog}</p>
         </div>
 
+        {/* Total Project */}
         <div className="bg-white rounded-2xl shadow p-6 hover:shadow-md transition">
           <h2 className="text-xl font-semibold mb-2">Total Project</h2>
           <p className="text-3xl font-bold text-green-500">
             {stats.totalProject}
-          </p>
-        </div>
-
-        <div className="bg-white rounded-2xl shadow p-6 hover:shadow-md transition">
-          <h2 className="text-xl font-semibold mb-2">Total Achievement</h2>
-          <p className="text-3xl font-bold text-purple-600">
-            {stats.totalAchievement}
           </p>
         </div>
       </div>
